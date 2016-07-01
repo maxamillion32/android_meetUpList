de643cdc69960e35744380633dfb2d9585224c75
themingfu <mwen82@gmail.com>
2016-07-01 11:44:30 -0700

    Log user input to logcat

--- app/src/main/java/com/epicodus/meetuplist/MainActivity.java
+++ app/src/main/java/com/epicodus/meetuplist/MainActivity.java
@@ -1,13 +1,32 @@
 package com.epicodus.meetuplist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindEventsButton;
+    private EditText mInterestEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mInterestEditText = (EditText) findViewById(R.id.InterestEditText);
+        mFindEventsButton = (Button) findViewById(R.id.FindEventsButton);
+        mFindEventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String interest = mInterestEditText.getText().toString();
+                Log.d(TAG, interest);
+                Intent intent = new Intent(MainActivity.this, UpcomingEventsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
