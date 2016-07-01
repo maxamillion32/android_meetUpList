e87f707b09f4138065d8ba8e596eed47412d8e4b
themingfu <mwen82@gmail.com>
2016-07-01 13:31:26 -0700

    Add custom font on main page

--- app/src/main/java/com/epicodus/meetuplist/MainActivity.java
+++ app/src/main/java/com/epicodus/meetuplist/MainActivity.java
@@ -1,17 +1,19 @@
 package com.epicodus.meetuplist;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindEventsButton;
     private EditText mInterestEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mInterestEditText = (EditText) findViewById(R.id.InterestEditText);
 
+        mAppNameTextView = (TextView) findViewById(R.id.AppNameTextView);
+        Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mAppNameTextView.setTypeface(pacificoFont);
         mFindEventsButton = (Button) findViewById(R.id.FindEventsButton);
         mFindEventsButton.setOnClickListener(this);
 
