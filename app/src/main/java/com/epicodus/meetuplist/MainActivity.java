7d3b44b30ac7632b3e5a508e9118082020fca88c
themingfu <mwen82@gmail.com>
2016-07-01 13:12:38 -0700

    Implement OnClickListener interface

--- app/src/main/java/com/epicodus/meetuplist/MainActivity.java
+++ app/src/main/java/com/epicodus/meetuplist/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindEventsButton;
     private EditText mInterestEditText;
@@ -18,16 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mInterestEditText = (EditText) findViewById(R.id.InterestEditText);
+
         mFindEventsButton = (Button) findViewById(R.id.FindEventsButton);
-        mFindEventsButton.setOnClickListener(new View.OnClickListener() {
+        mFindEventsButton.setOnClickListener(this);
+
+    }
+
             @Override
             public void onClick(View v) {
+                if (v == mFindEventsButton) {
                 String interest = mInterestEditText.getText().toString();
-                Log.d(TAG, interest);
                 Intent intent = new Intent(MainActivity.this, UpcomingEventsActivity.class);
                 intent.putExtra("interest", interest);
                 startActivity(intent);
             }
-        });
     }
 }
