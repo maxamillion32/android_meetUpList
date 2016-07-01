6213b77879b2ceea97bdbb92534ec5a5c45c6ac9
themingfu <mwen82@gmail.com>
2016-07-01 14:07:54 -0700

    Add log in button and page

--- app/src/main/java/com/epicodus/meetuplist/UpcomingEventsActivity.java
+++ app/src/main/java/com/epicodus/meetuplist/UpcomingEventsActivity.java
@@ -3,7 +3,9 @@ package com.epicodus.meetuplist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ public class UpcomingEventsActivity extends AppCompatActivity {
     private ListView mListView;
     private String[] events = new String[] {"The Portland Hiking Meetup Group", "Docker Portland, OR",
             "New-to-Portland Moms", "Free Yoga for Adults in Portland Area", "Activities and Sports for the Awesomely Mediocre", "New Relic FutureTalks PDX", "Art Geeks"};
+    private Button mLogInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,14 @@ public class UpcomingEventsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String interest = intent.getStringExtra("interest");
         mInterestTextView.setText("Here are all the results for: " + interest);
+
+        mLogInButton = (Button) findViewById(R.id.LogInButton);
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UpcomingEventsActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
