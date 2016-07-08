9f2651827b1760b97372f30193ccfcb349b11321
themingfu <mwen82@gmail.com>
2016-07-07 19:43:50 -0700

    Add API Key to gradle.properties and Constants.java

---
+++ app/src/main/java/com/epicodus/meetuplist/Constants.java
@@ -0,0 +1,14 @@
+package com.epicodus.meetuplist;
+
+/**
+ * Created by Guest on 7/7/16.
+ */
+public class Constants {
+    public static final String MOVIE_API_KEY = BuildConfig.MOVIE_API_KEY;
+
+    public static final String BASE_URL = "https://api.meetup.com/2/open_events?topic=photo&time=,1w&";
+    public static final String API_QUERY_PARAMETER = "api_key";
+    public static final String SEARCH_QUERY_PARAMETER = "query";
+
+    // https://api.meetup.com/2/open_events?topic=meditation&time=,1w&key=263238565a442e1967147767292e2462
+}
