0e2a0853c47b841d7c3ff1aea060486dc9cef4b9
themingfu <mwen82@gmail.com>
2016-07-07 20:15:48 -0700

    Add MeetupService class

--- app/src/main/java/com/epicodus/meetuplist/Constants.java
+++ app/src/main/java/com/epicodus/meetuplist/Constants.java
@@ -4,11 +4,11 @@ package com.epicodus.meetuplist;
  * Created by Guest on 7/7/16.
  */
 public class Constants {
-    public static final String MOVIE_API_KEY = BuildConfig.MOVIE_API_KEY;
+    public static final String MEETUP_API_KEY = BuildConfig.MEETUP_API_KEY;
 
-    public static final String BASE_URL = "https://api.meetup.com/2/open_events?topic=photo&time=,1w&";
-    public static final String API_QUERY_PARAMETER = "api_key";
-    public static final String SEARCH_QUERY_PARAMETER = "query";
+    public static final String BASE_URL = "https://api.meetup.com/2/open_events";
+    public static final String API_QUERY_PARAMETER = "key";
+    public static final String SEARCH_QUERY_PARAMETER = "topic";
 
     // https://api.meetup.com/2/open_events?topic=meditation&time=,1w&key=263238565a442e1967147767292e2462
 }
---
+++ app/src/main/java/com/epicodus/meetuplist/MeetupService.java
@@ -0,0 +1,31 @@
+package com.epicodus.meetuplist;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Guest on 7/7/16.
+ */
+public class MeetupService {
+    public static final String TAG = UpcomingEventsActivity.class.getSimpleName()
+
+    public static void findMeetups(String location, Callback callback) {
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.SEARCH_QUERY_PARAMETER, topic);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+}
